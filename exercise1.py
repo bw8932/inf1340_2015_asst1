4903406ac743ca38216fb9b8ca16203f4836dc03
HonourCode <jake.miller@mail.utoronto.ca>
2015-10-01 19:26:41 -0400

    Percentage change, print stages query

--- exercise1.py
+++ exercise1.py
@@ -12,14 +12,14 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-#test to see if pycharm is configured to push to github
 #first calculate the purchase details
 shares_bought = 2000
 price_bought = 900
 share_expense = price_bought * shares_bought
-commission_bought = .3 * share_expense
+commission_bought = .03 * share_expense
 total_expense = share_expense + commission_bought
 
+#do we include these as printed?
 print (share_expense)
 print (total_expense)
 
@@ -28,9 +28,10 @@ print (total_expense)
 shares_sold = 2000
 price_sold = 942.75
 sale_income = shares_sold * price_sold
-commission_sold = .3 * sale_income
+commission_sold = .03 * sale_income
 total_sale_income = sale_income - commission_sold
 
+#again should be print these or just the final result?
 print (sale_income)
 print (total_sale_income)
 
@@ -42,5 +43,6 @@ print ("Lakshmi's investing netted her $%s") % (money)
 
 #initial copy had these lines... that's why I called her overall outcome variable "money"
 #does this mean we're to subtract that final net from a $1000 initial amount???
+#We should ask.
     #money = 1000.00
     #print(money)
