eddf2942b5b6a98af232748f675f6ef6f1aa6255
Brady Williamson <brady.williamson@gmail.com>
2015-10-07 19:11:02 -0400

    exercise 2 functional

--- exercise2.py
+++ exercise2.py
@@ -24,12 +24,32 @@ def name_that_shape():
     Errors:
 
     """
-    #initial test code to see if case of square works and error message works
-    sides=raw_input("How many sides does the shape have?")
-    if sides==("4"):
-        print("That's a square!")
+
+    sides = int(raw_input("How many sides does the polygon have?"))
+    if sides == 1:
+        print("Line")
+    elif sides == 2:
+        print ("Angle")
+    elif sides == 3:
+        print ("Triangle")
+    elif sides == 4:
+        print ("Quadrangle")
+    elif sides == 5:
+        print ("Pentagon")
+    elif sides == 6:
+        print ("Hexagon")
+    elif sides == 7:
+        print ("Septagon")
+    elif sides == 8:
+        print ("Octagon")
+    elif sides == 9:
+        print ("Nonagon")
+    elif sides == 10:
+        print ("Decagon")
     else:
-        print("Error")
+        print ("Error")
+        name_that_shape()
+
 
 
 name_that_shape()
